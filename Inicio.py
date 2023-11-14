65ed645cd3c251952f53d3ac41e081924bfd753a
Marycielo <mberrioz@unal.edu.co>
2023-11-14 15:42:48 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,51 +1,34 @@
-# Inicio.py
-
-# Resto de las importaciones
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-# Configuración de la página
-st.set_page_config(page_title="Análisis de Datos con Streamlit", layout="wide")
-
-# Título de la aplicación
+# Información sobre la aplicación
 st.title("Análisis de Datos con Numpy, Pandas y Matplotlib")
+st.write(
+    "Bienvenido a nuestra aplicación de análisis de datos. "
+    "Esta aplicación utiliza Numpy, Pandas y Matplotlib para realizar análisis estadísticos simples y visualizar tendencias en los datos."
+)
 
-# Sección de carga de datos
-uploaded_file = st.file_uploader("Cargar archivo CSV o Excel", type=["csv", "xlsx"])
+# Información de contacto de los desarrolladores
+st.header("Contacto de los Desarrolladores")
+st.write(
+    "Desarrollado por:\n"
+    "- [Tu Nombre]\n"
+    "- [Otro Desarrollador, si es el caso]\n"
+    "Para consultas, contáctenos en: [correo electrónico]\n"
+)
 
-if uploaded_file is not None:
-    # Cargar datos con Pandas
-    df = pd.read_csv(uploaded_file)  # Puedes cambiar a pd.read_excel() si es un archivo Excel
+# Información adicional o instrucciones para el usuario si es necesario
+st.header("Instrucciones de Uso")
+st.write(
+    "1. Cargue sus datos utilizando la opción de carga de archivos.\n"
+    "2. Realice análisis estadísticos con las herramientas proporcionadas.\n"
+    "3. Visualice tendencias utilizando los gráficos generados.\n"
+)
 
-    # Mostrar el conjunto de datos
-    st.subheader("Conjunto de Datos")
-    st.write(df)
+# Puedes agregar más secciones según sea necesario
 
-    # Manipulación de datos con Numpy y Pandas
-    # (Aquí puedes agregar las manipulaciones según tus necesidades)
-
-    # Cálculos estadísticos
-    st.subheader("Cálculos Estadísticos")
-    st.write("Media:", df.mean())
-    st.write("Mediana:", df.median())
-    st.write("Desviación Estándar:", df.std())
-    st.write("Mínimo:", df.min())
-    st.write("Máximo:", df.max())
-
-    # Visualización de datos con Matplotlib
-    st.subheader("Visualización de Datos")
-
-    # Ejemplo de gráfico de barras
-    st.bar_chart(df)
-
-    # (Puedes agregar más tipos de gráficos según sea necesario)
-
-    # Nota: Asegúrate de cerrar el gráfico para evitar superposiciones
-    plt.close()
-
-    # Presentación de resultados en la interfaz de usuario
-    st.subheader("Resultados")
-
-    # (Aquí puedes presentar más resultados según tus necesidades)
+# Código del desarrollador
+st.header("Desarrolladores")
+st.write(
+    "Esta aplicación fue desarrollada por [Tu Nombre] y [Otro Desarrollador]."
+    " Si tiene alguna pregunta o sugerencia, no dude en contactarnos."
+)
